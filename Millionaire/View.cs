e875219abd89aa59b05481e6bec76fd14b11f477
Dekacc <dekacc@outlook.com>
2016-04-10 17:55:29 +0200

    Made the menu and game in one window

--- Millionaire/View.cs
+++ Millionaire/View.cs
@@ -12,13 +12,18 @@ namespace Millionaire
 {
     public partial class View : Form
     {
-        Model model;
+        private Model model;
 
         public View()
         {
             InitializeComponent();
-            model = new Model();
+        }
 
+        private void newGame_btn_Click(object sender, EventArgs e)
+        {
+            playPanel.Show();
+
+            model = new Model();
             updateQuestion();
         }
 
@@ -27,28 +32,39 @@ namespace Millionaire
             //takes the current question from model and displays it
             Question q = model.currentQuestion;
             questionLabel.Text = q.question;
-            button1.Text = q.answer[0];
-            button2.Text = q.answer[1];
-            button3.Text = q.answer[2];
-            button4.Text = q.answer[3];
+            answer1.Text = q.answer[0];
+            answer2.Text = q.answer[1];
+            answer3.Text = q.answer[2];
+            answer4.Text = q.answer[3];
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void info_btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("Кој сака да биде милионер ?\nИзработија:\nДавид Симеоновски\nНикола Танев"), "Кој сака да биде милионер ?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void answer1_Click(object sender, EventArgs e)
         {
             model.tryAnswer(0);
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void answer2_Click(object sender, EventArgs e)
         {
             model.tryAnswer(1);
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void answer3_Click(object sender, EventArgs e)
         {
             model.tryAnswer(2);
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void answer4_Click(object sender, EventArgs e)
         {
             model.tryAnswer(3);
         }
