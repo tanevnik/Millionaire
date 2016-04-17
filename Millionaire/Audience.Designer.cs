2f199bcb58c6d8ca6f0047433855403743db3479
Dekacc <dekacc@outlook.com>
2016-04-17 20:25:11 +0200

    Add the code for the visual representation of the audience joker

--- Millionaire/Audience.Designer.cs
+++ Millionaire/Audience.Designer.cs
@@ -35,14 +35,14 @@
             this.labelG = new System.Windows.Forms.Label();
             this.close = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.label0 = new System.Windows.Forms.Label();
+            this.bar3 = new System.Windows.Forms.Panel();
+            this.bar2 = new System.Windows.Forms.Panel();
+            this.bar1 = new System.Windows.Forms.Panel();
+            this.bar0 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -107,16 +107,16 @@
             // panel1
             // 
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
-            this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.panel5);
-            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.label0);
+            this.panel1.Controls.Add(this.bar3);
+            this.panel1.Controls.Add(this.bar2);
             this.panel1.Controls.Add(this.labelG);
-            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.bar1);
             this.panel1.Controls.Add(this.labelV);
-            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.bar0);
             this.panel1.Controls.Add(this.labelB);
             this.panel1.Controls.Add(this.labelA);
             this.panel1.Location = new System.Drawing.Point(13, 13);
@@ -124,25 +124,14 @@
             this.panel1.Size = new System.Drawing.Size(160, 200);
             this.panel1.TabIndex = 5;
             // 
-            // label4
-            // 
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(122, 3);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(30, 23);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "100";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(84, 3);
+            this.label3.Location = new System.Drawing.Point(122, 3);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(30, 23);
-            this.label3.TabIndex = 6;
+            this.label3.TabIndex = 7;
             this.label3.Text = "100";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -150,10 +139,10 @@
             // 
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(46, 3);
+            this.label2.Location = new System.Drawing.Point(84, 3);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 23);
-            this.label2.TabIndex = 5;
+            this.label2.TabIndex = 6;
             this.label2.Text = "100";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -161,44 +150,55 @@
             // 
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(8, 3);
+            this.label1.Location = new System.Drawing.Point(46, 3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(30, 23);
-            this.label1.TabIndex = 4;
+            this.label1.TabIndex = 5;
             this.label1.Text = "100";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panel5
+            // label0
             // 
-            this.panel5.BackColor = System.Drawing.Color.DarkGreen;
-            this.panel5.Location = new System.Drawing.Point(122, 25);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(30, 150);
-            this.panel5.TabIndex = 1;
+            this.label0.BackColor = System.Drawing.Color.Transparent;
+            this.label0.ForeColor = System.Drawing.Color.White;
+            this.label0.Location = new System.Drawing.Point(8, 3);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(30, 23);
+            this.label0.TabIndex = 4;
+            this.label0.Text = "100";
+            this.label0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panel4
+            // bar3
             // 
-            this.panel4.BackColor = System.Drawing.Color.DarkGreen;
-            this.panel4.Location = new System.Drawing.Point(84, 25);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(30, 150);
-            this.panel4.TabIndex = 1;
+            this.bar3.BackColor = System.Drawing.Color.DarkGreen;
+            this.bar3.Location = new System.Drawing.Point(122, 26);
+            this.bar3.Name = "bar3";
+            this.bar3.Size = new System.Drawing.Size(30, 150);
+            this.bar3.TabIndex = 1;
             // 
-            // panel3
+            // bar2
             // 
-            this.panel3.BackColor = System.Drawing.Color.DarkGreen;
-            this.panel3.Location = new System.Drawing.Point(46, 25);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(30, 150);
-            this.panel3.TabIndex = 1;
+            this.bar2.BackColor = System.Drawing.Color.DarkGreen;
+            this.bar2.Location = new System.Drawing.Point(84, 26);
+            this.bar2.Name = "bar2";
+            this.bar2.Size = new System.Drawing.Size(30, 150);
+            this.bar2.TabIndex = 1;
             // 
-            // panel2
+            // bar1
             // 
-            this.panel2.BackColor = System.Drawing.Color.DarkGreen;
-            this.panel2.Location = new System.Drawing.Point(8, 25);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(30, 150);
-            this.panel2.TabIndex = 0;
+            this.bar1.BackColor = System.Drawing.Color.DarkGreen;
+            this.bar1.Location = new System.Drawing.Point(46, 26);
+            this.bar1.Name = "bar1";
+            this.bar1.Size = new System.Drawing.Size(30, 150);
+            this.bar1.TabIndex = 1;
+            // 
+            // bar0
+            // 
+            this.bar0.BackColor = System.Drawing.Color.DarkGreen;
+            this.bar0.Location = new System.Drawing.Point(8, 26);
+            this.bar0.Name = "bar0";
+            this.bar0.Size = new System.Drawing.Size(30, 150);
+            this.bar0.TabIndex = 0;
             // 
             // Audience
             // 
@@ -223,13 +223,13 @@
         private System.Windows.Forms.Label labelG;
         private System.Windows.Forms.Button close;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel bar3;
+        private System.Windows.Forms.Panel bar2;
+        private System.Windows.Forms.Panel bar1;
+        private System.Windows.Forms.Panel bar0;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label0;
     }
 }
--- Millionaire/Audience.cs
+++ Millionaire/Audience.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Millionaire
@@ -7,16 +8,36 @@ namespace Millionaire
     {
         public Audience(int[] votes)
         {
+            int maxBarHeight = 150;
+
             InitializeComponent();
-            label1.Text = votes[0] + "%";
-            label2.Text = votes[1] + "%";
-            label3.Text = votes[2] + "%";
-            label4.Text = votes[3] + "%";
+            label0.Text = votes[0] + "%";
+            label1.Text = votes[1] + "%";
+            label2.Text = votes[2] + "%";
+            label3.Text = votes[3] + "%";
+
+            //TODO add animation of bars rising!
+
+            //Calculating the heights of the bars
+            int[] barHeight = new int[4];
+            for(int i = 0; i<4; i++) barHeight[i] = votes[i] * maxBarHeight / 100;
+
+            //Setting the height of the bars
+            bar0.Height = barHeight[0];
+            bar1.Height = barHeight[1];
+            bar2.Height = barHeight[2];
+            bar3.Height = barHeight[3];
+
+            //Setting the Y location of the bars
+            bar0.Location = new Point(bar0.Location.X, bar0.Location.Y + (maxBarHeight - votes[0] * maxBarHeight / 100));
+            bar1.Location = new Point(bar1.Location.X, bar1.Location.Y + (maxBarHeight - votes[1] * maxBarHeight / 100));
+            bar2.Location = new Point(bar2.Location.X, bar2.Location.Y + (maxBarHeight - votes[2] * maxBarHeight / 100));
+            bar3.Location = new Point(bar3.Location.X, bar3.Location.Y + (maxBarHeight - votes[3] * maxBarHeight / 100));
         }
 
         private void close_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
     }
 }
