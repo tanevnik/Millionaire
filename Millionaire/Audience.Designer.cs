e64ff0c53385315fa202707cc413257994a96daf
Dekacc <dekacc@outlook.com>
2016-04-17 10:58:30 +0200

    Add background and vertical bars to audience joker

--- Millionaire/Audience.Designer.cs
+++ Millionaire/Audience.Designer.cs
@@ -28,91 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Audience));
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelV = new System.Windows.Forms.Label();
+            this.labelG = new System.Windows.Forms.Label();
             this.close = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // labelA
             // 
-            this.label1.Location = new System.Drawing.Point(12, 188);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.labelA.BackColor = System.Drawing.Color.Transparent;
+            this.labelA.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(155)))), ((int)(((byte)(28)))));
+            this.labelA.Location = new System.Drawing.Point(5, 179);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(36, 20);
+            this.labelA.TabIndex = 0;
+            this.labelA.Text = "A";
+            this.labelA.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // label2
+            // labelB
             // 
-            this.label2.Location = new System.Drawing.Point(54, 188);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 23);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.labelB.BackColor = System.Drawing.Color.Transparent;
+            this.labelB.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(155)))), ((int)(((byte)(28)))));
+            this.labelB.Location = new System.Drawing.Point(43, 179);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(36, 20);
+            this.labelB.TabIndex = 1;
+            this.labelB.Text = "Б";
+            this.labelB.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // label3
+            // labelV
             // 
-            this.label3.Location = new System.Drawing.Point(96, 188);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(36, 23);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.labelV.BackColor = System.Drawing.Color.Transparent;
+            this.labelV.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelV.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(155)))), ((int)(((byte)(28)))));
+            this.labelV.Location = new System.Drawing.Point(81, 179);
+            this.labelV.Name = "labelV";
+            this.labelV.Size = new System.Drawing.Size(36, 20);
+            this.labelV.TabIndex = 2;
+            this.labelV.Text = "В";
+            this.labelV.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // label4
+            // labelG
             // 
-            this.label4.Location = new System.Drawing.Point(138, 188);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 23);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.labelG.BackColor = System.Drawing.Color.Transparent;
+            this.labelG.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelG.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(155)))), ((int)(((byte)(28)))));
+            this.labelG.Location = new System.Drawing.Point(119, 179);
+            this.labelG.Name = "labelG";
+            this.labelG.Size = new System.Drawing.Size(36, 20);
+            this.labelG.TabIndex = 3;
+            this.labelG.Text = "Г";
+            this.labelG.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // close
             // 
-            this.close.Location = new System.Drawing.Point(13, 215);
+            this.close.Location = new System.Drawing.Point(13, 223);
             this.close.Name = "close";
-            this.close.Size = new System.Drawing.Size(161, 23);
+            this.close.Size = new System.Drawing.Size(160, 23);
             this.close.TabIndex = 4;
             this.close.Text = "Затвори";
             this.close.UseVisualStyleBackColor = true;
             this.close.Click += new System.EventHandler(this.close_Click);
             // 
-            // label5
+            // panel1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(33, 85);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(122, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "TODO: add vertical bars";
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.labelG);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.labelV);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.labelB);
+            this.panel1.Controls.Add(this.labelA);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 200);
+            this.panel1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(122, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "100";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(84, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "100";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(46, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "100";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "100";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel5.Location = new System.Drawing.Point(122, 25);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(30, 150);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel4.Location = new System.Drawing.Point(84, 25);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(30, 150);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel3.Location = new System.Drawing.Point(46, 25);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(30, 150);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel2.Location = new System.Drawing.Point(8, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(30, 150);
+            this.panel2.TabIndex = 0;
             // 
             // Audience
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(190, 251);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(190, 254);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.close);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "Audience";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Publika";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label labelV;
+        private System.Windows.Forms.Label labelG;
         private System.Windows.Forms.Button close;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
